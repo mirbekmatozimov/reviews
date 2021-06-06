f1b031ae75fb276030efde8bb18baac86aabfe31
mirbek <mirbek.25.52@gmail.com>
2021-06-06 16:59:00 +0600

    Updates

---
+++ precache-manifest.3423ac02eb97ab41db057039601f9fe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631f31486f8f37145b56893bc3ba77ea",
+    "url": "/mirbekmatozimov/reviews.git/index.html"
+  },
+  {
+    "revision": "5bd3659d0b254e9fd36b",
+    "url": "/mirbekmatozimov/reviews.git/static/css/main.2f876791.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/mirbekmatozimov/reviews.git/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mirbekmatozimov/reviews.git/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd3659d0b254e9fd36b",
+    "url": "/mirbekmatozimov/reviews.git/static/js/main.8d87273f.chunk.js"
+  },
+  {
+    "revision": "7ecf832e7f0cf8e03ad7",
+    "url": "/mirbekmatozimov/reviews.git/static/js/runtime-main.6c3f49ae.js"
+  }
+]);
