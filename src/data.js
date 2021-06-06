8becf0d6f2a6d716a58308b7edaaca943fe00533
mirbek <mirbek.25.52@gmail.com>
2021-06-06 16:53:28 +0600

    deploy

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Lily White',
+    job: 'web developer',
+    image:
+      'https://randomuser.me/api/portraits/women/96.jpg',
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'Lori Kim',
+    job: 'web designer',
+    image:
+      'https://randomuser.me/api/portraits/women/76.jpg',
+    text:
+      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'Jose Peck',
+    job: 'intern',
+    image:
+      'https://randomuser.me/api/portraits/men/24.jpg',
+    text:
+      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'Riley Nichols',
+    job: 'the boss',
+    image:
+      'https://randomuser.me/api/portraits/women/85.jpg',
+    text:
+      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+
+export default reviews;
